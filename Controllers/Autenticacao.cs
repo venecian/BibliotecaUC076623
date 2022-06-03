57a34dffa8ecd04cc611f14e6897ff497c9617f1
Luthiano Venecian <venecian@gmail.com>
2022-06-03 11:22:18 -0300

    Autenticação de usuários

--- Controllers/Autenticacao.cs
+++ Controllers/Autenticacao.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-
+using Biblioteca.Models;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Biblioteca.Controllers
 {
@@ -8,10 +10,65 @@ namespace Biblioteca.Controllers
     {
         public static void CheckLogin(Controller controller)
         {   
-            if(string.IsNullOrEmpty(controller.HttpContext.Session.GetString("user")))
+            if(string.IsNullOrEmpty(controller.HttpContext.Session.GetString("login")))
             {
                 controller.Request.HttpContext.Response.Redirect("/Home/Login");
             }
         }
+
+        public static bool verificaLoginSenha(string login, string senha, Controller controller){
+
+            using(BibliotecaContext bc = new BibliotecaContext()){
+            
+                verificaSeUsuarioAdminExiste(bc);
+
+                senha = Criptografia.TextoCriptografado(senha);
+
+                IQueryable<Usuario> UsuarioEncontrado = bc.Usuarios.Where(u => u.Login == login && u.Senha == senha);
+                List<Usuario> ListaUsuarioEncontrado = UsuarioEncontrado.ToList();
+
+                if (ListaUsuarioEncontrado.Count==0) {
+                    return false;
+                } else {
+                    
+                    //adicionando na sessao da controller encaminhada, dado do usuario localizado
+                    controller.HttpContext.Session.SetString("login",ListaUsuarioEncontrado[0].Login);
+                    controller.HttpContext.Session.SetString("nome",ListaUsuarioEncontrado[0].Nome);
+                    controller.HttpContext.Session.SetInt32("tipo",ListaUsuarioEncontrado[0].Tipo);
+                    
+                    return true;
+                }
+
+            }
+           
+        }
+
+        public static void verificaSeUsuarioAdminExiste(BibliotecaContext bc){
+
+            IQueryable<Usuario> UsuarioEncontrado = bc.Usuarios.Where(u => u.Login == "admin");
+
+            if (UsuarioEncontrado.ToList().Count==0) {
+                //criar o usuario admin   
+
+                Usuario admin = new Usuario();
+                admin.Nome = "Administrador";
+                admin.Login = "admin";
+                admin.Senha = Criptografia.TextoCriptografado("123");
+                admin.Tipo = Usuario.ADMIN;
+
+                bc.Usuarios.Add(admin);
+                bc.SaveChanges();
+            }
+
+
+        }
+
+        public static void verificaSeUsuarioEAdmin(Controller controller){
+            if (!(controller.HttpContext.Session.GetInt32("tipo") == Usuario.ADMIN)) {
+               controller.Request.HttpContext.Response.Redirect("/Usuarios/NeedAdmin"); 
+            }
+        }    
+
+
     }
 }
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,6 +22,7 @@ namespace Biblioteca.Controllers
         public IActionResult Index()
         {
             Autenticacao.CheckLogin(this);
+            ViewData["mensagem"] = "seja bem vindo ao sistema de controle de empréstimo de livros";
             return View();
         }
 
@@ -33,15 +34,12 @@ namespace Biblioteca.Controllers
         [HttpPost]
         public IActionResult Login(string login, string senha)
         {
-            if(login != "admin" || senha != "123")
-            {
+            
+            if (Autenticacao.verificaLoginSenha(login,senha,this)) {
+                return RedirectToAction("Index");
+            } else {    
                 ViewData["Erro"] = "Senha inválida";
                 return View();
-            }
-            else
-            {
-                HttpContext.Session.SetString("user", "admin");
-                return RedirectToAction("Index");
             }
         }
 
